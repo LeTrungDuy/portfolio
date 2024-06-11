b2f2b804d881029b52fab81e4aafe6d5cd0622aa
Duy Le <letrungduy11496@gmail.com>
2024-06-11 17:37:46 +0700

    chore: remove code redudant

--- src/components/template/layout/index.tsx
+++ src/components/template/layout/index.tsx
@@ -5,12 +5,8 @@ interface ILayout {
   children: ReactNode;
 }
 
-const Layout = ({children}: ILayout) => {
-  return (
-    <div className="portfolio-layout">
-      {children}
-    </div>>
-  );
+const Layout = ({ children }: ILayout) => {
+  return <div className="portfolio-layout">{children}</div>;
 };
 
 export default Layout;
